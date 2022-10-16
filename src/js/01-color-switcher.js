ff912e6f85c7e6af82f2fd17a1569db5c79dd567
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-16 19:07:48 +0200

    Zadanie 1 wykonane

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+stopButton.disabled = true;
+
+startButton.addEventListener("click", () => {
+    body.style.backgroundColor = getRandomHexColor();
+    intervalID = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+});
+stopButton.addEventListener("click", () => {
+    stopButton.disabled = true;
+    startButton.disabled = false;
+    clearInterval(intervalID);
+})
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
