0e6eed17eb99dddbb7ac9dcff0fc371b44e7cefc
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-16 20:34:44 +0200

    Update 02-timer.js

---
+++ src/js/02-timer.js
@@ -0,0 +1,3 @@
+const inputDate = document.querySelector('.datetime-picker');
+const startButton = document.querySelector('button[data-start]');
+const countdownTimer = document.querySelector('.timer');
