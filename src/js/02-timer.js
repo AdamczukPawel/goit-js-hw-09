18c663b95dc1ee576cf92c5dee06ef4a4468e76a
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-17 22:15:24 +0200

    Wykonanie zadania 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,13 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const inputDate = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
-const countdownTimer = document.querySelector('.timer');
+const daysTimer = document.querySelector('.value[data-days]');
+const hoursTimer = document.querySelector('.value[data-hours]');
+const minutesTimer = document.querySelector('.value[data-minutes]');
+const secondsTimer = document.querySelector('.value[data-seconds]');
 
 startButton.disabled = true;
 
@@ -15,13 +19,55 @@ const options = {
   onClose(selectedDates) {
     const currentDate = new Date();
     if (selectedDates[0].getTime() < currentDate.getTime()) {
-        window.alert("Please choose a date in the future");
-        startButton.disabled = true;
+      Notiflix.Notify.failure("Please choose a date in the future");
+      startButton.disabled = true;
     } else {
-        startButton.disabled = false;
+      startButton.disabled = false;
     }
     console.log(selectedDates[0]);
   },
 };
 
-flatpickr(inputDate, options);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+let timeRemaining = 0;
+const inputedDate = flatpickr(inputDate, options);
+
+startButton.addEventListener('click', () => {
+  setInterval(() => {
+    timeRemaining = convertMs(inputedDate.selectedDates[0].getTime() - new Date().getTime());
+    daysTimer.innerHTML = timeRemaining.days;
+    hoursTimer.innerHTML = timeRemaining.hours;
+    minutesTimer.innerHTML = timeRemaining.minutes;
+    secondsTimer.innerHTML = timeRemaining.seconds;
+    startButton.disabled = true;
+    inputDate.disabled = true;
+    function addLeadingZero() {
+      if (timeRemaining.days <= 9 || timeRemaining.hours <= 9 || timeRemaining.minutes <= 9 || timeRemaining.seconds <= 9) {
+        daysTimer.innerHTML = daysTimer.textContent.padStart(2, '0');
+        hoursTimer.innerHTML = hoursTimer.textContent.padStart(2, '0');
+        minutesTimer.innerHTML = minutesTimer.textContent.padStart(2, '0');
+        secondsTimer.innerHTML = secondsTimer.textContent.padStart(2, '0');
+      }
+    }
+    addLeadingZero();
+  },1000)
+})
+
