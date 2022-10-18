aa914ac6d3cf622650829c71ca8dfaa295da1d96
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-18 22:17:22 +0200

    Początek zadania 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,18 @@
+const firstDelay = document.querySelector('input[name=delay]');
+const delayStep = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount]');
+const createPromisesButton = document.querySelector('button');
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+  })
+};
