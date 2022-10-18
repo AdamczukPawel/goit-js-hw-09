8937b03058a180a9f8d20f66e1c58eb3afecf258
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-18 20:17:48 +0000

    Deploying to gh-pages from @ AdamczukPawel/goit-js-hw-09@aa914ac6d3cf622650829c71ca8dfaa295da1d96 🚀

---
+++ 03-promises.d1d8a9ff.js
@@ -0,0 +1,2 @@
+document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]"),document.querySelector("button");
+//# sourceMappingURL=03-promises.d1d8a9ff.js.map
